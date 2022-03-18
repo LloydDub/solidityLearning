d173763b9c23cac7b0077417e0911668dc0f1296
LloydDub <lloydrww@gmail.com>
2022-03-18 00:51:43 -0600

    structs and arrays in Solidity

--- contractTinkering.sol
+++ contractTinkering.sol
@@ -9,5 +9,17 @@ contract ZombieFactory {
   uint dnaDigits = 16;
   uint dnaModulus = 10 ** dnaDigits;
 
+   // Structs allow you to create more complicated data types that have multiple properties.
+  struct Zombie  {
+        string name;
+        uint dna;
+    }
+
+
+      //There are two types of arrays in Solidity: fixed arrays and dynamic arrays:
+
+      //You can declare an array as public, and Solidity will automatically create a getter method for it. The syntax looks like:
+    Zombie[] public zombies;
+
 
 }
