06360fae492d66826ef8c1c1b6c856ca55e0cc8d
LloydDub <lloydrww@gmail.com>
2022-03-17 20:35:46 -0600

    Learning Solidity, Watch out Vitalik

---
+++ contractTinkering.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.16 <0.9.0;
+
+// State variables are permanently stored in contract storage. This means they're written to the Ethereum blockchain. Think of them like writing to a DB.
+
+
+contract ZombieFactory {
+
+  uint dnaDigits = 16;
+  uint dnaModulus = 10 ** dnaDigits;
+
+
+}
