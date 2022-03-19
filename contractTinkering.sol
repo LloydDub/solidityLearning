8693af2a3e66ee4ba18cd0575caeb6a96239a71d
LloydDub <lloydrww@gmail.com>
2022-03-18 20:15:08 -0600

    Passing arguments in solidity

--- contractTinkering.sol
+++ contractTinkering.sol
@@ -21,5 +21,16 @@ contract ZombieFactory {
       //You can declare an array as public, and Solidity will automatically create a getter method for it. The syntax looks like:
     Zombie[] public zombies;
 
+    function createZombie (string memory _name, uint _dna) public {
+        // start here
+    }
+
 
 }
+
+// here are two ways in which you can pass an argument to a Solidity function:
+
+// By value, which means that the Solidity compiler creates a new copy of the parameter's value and passes it to your 
+// function. This allows your function to modify the value without worrying that the value of the initial parameter gets changed.
+// By reference, which means that your function is called with a... reference to the original variable. Thus, if your
+//  function changes the value of the variable it receives, the value of the original variable gets changed.
